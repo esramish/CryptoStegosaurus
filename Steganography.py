127f909332ed913e556b7bc83e793ab004805cfb
drewshki <d_shippey@coloradocollege.edu>
2019-05-10 16:58:04 -0600

    We did it reddit
    Yeah we did

--- Steganography.py
+++ Steganography.py
@@ -1,7 +1,7 @@
 import random
-import scipy
 from PIL import Image
 import numpy as np
+import bitarray
 
 # This program currently represents the least significant bit (of a given byte) first
 
@@ -9,13 +9,9 @@ BITS_MODIFIED_PER_PIXEL = 1
 
 
 def convert_msg_to_bits(msg):
-    bits = []
-    for char in msg: 
-        val = ord(char)
-        for i in range(8):
-            bit_as_bool = not not (val & (2**i))
-            bits.append(bit_as_bool * 1)
-    return bits
+    ba = bitarray.bitarray()
+    ba.frombytes(msg.encode('utf-8'))
+    return ba
 
 
 def encode_msg(msg_bits, img_bits):
@@ -25,12 +21,11 @@ def encode_msg(msg_bits, img_bits):
     for i in range(len(msg_bits)):
         bit_to_encode = msg_bits[i]
         img_bits[ (i // BITS_MODIFIED_PER_PIXEL * 8) + (i % BITS_MODIFIED_PER_PIXEL)] = bit_to_encode
+    print(img_bits)
     return img_bits
 
 def getPixelList(image):
-
     pixel_array = np.array(image)
-    print (pixel_array[0][0][0])
     return pixel_array
 
 
@@ -58,6 +53,33 @@ def createEncodedImage(pixel_list):
     image = Image.fromarray(pixel_list)
     image.save("cat_new.png")
 
+def decodeImage(image):
+    pixel_list = getPixelList(image)
+    width = len(pixel_list[0])
+    index = 0
+    bit_list = []
+    word_len = len("nukes in cuba")
+    str_bits = ""
+    for i in range(8*word_len):
+        index_x = (index + i) % width
+        index_y = (index + i) // width
+        parity = pixel_list[index_y][index_x][0] % 2
+        str_bits += str(parity)
+        bit_list.append(parity)
+    return bit_list
+
+def decodeMessege(bit_list):
+
+    ba = bitarray.bitarray()
+    for i in bit_list:
+
+        ba.append(i)
+    return_string = ba.tobytes().decode('utf-8')
+    print(return_string)
+
+
+
+
 
 
 def main():
@@ -74,10 +96,13 @@ def main():
     msg_bits = convert_msg_to_bits(msg)
     pixel_list_modified = insertMessege(pixel_list_unModified, msg_bits)
     print()
-
     createEncodedImage(pixel_list_modified)
-    print("Message bits: " + str(msg_bits))
-    print("Array with message encoded inside: " + str(encode_msg(msg_bits, img_array)))
+    image2 = Image.open("cat_new.png")
+    bit_list = decodeImage(image2)
+    decodeMessege(bit_list)
+
+    # print("Message bits: " + str(msg_bits))
+    # print("Array with message encoded inside: " + str(encode_msg(msg_bits, img_array)))
 
 
 if __name__ == "__main__":
